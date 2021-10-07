188b3144506b2e062273c823252fc44a1299b402
Vaniiis <92022447+Vaniiis@users.noreply.github.com>
2021-10-07 09:00:22 +0200

    Add files via upload

---
+++ The provider/theprovider.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("color");
+ }
+
+
+
+
+ 
+ 
