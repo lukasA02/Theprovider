8fa3db328676c10d5d47751829e8848b93a15436
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-10-07 13:12:24 +0200

    Add files via upload

--- loggain.php
+++ loggain.php
@@ -1,5 +1,7 @@
 <?php
-require_once 'connect.php';
+session_start();
+require_once 'anslut.php';
+
 $username = $_POST['user'];
 $password = $_POST['pass'];
 
@@ -8,13 +10,13 @@ $sql = "SELECT * FROM anvandare WHERE Anvnamn = ? && Losen = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("ss", $username, $password);
 
-if($stmt->execute()){
+if ($stmt->execute()) {
     $result = $stmt->get_result();
     $num_rows = $result->num_rows;
 }
 
-if($num_rows > 0){
-    echo "Inloggad";
+if ($num_rows > 0) {
+    $_SESSION['inloggad'] = true;
 } else {
     echo "Inte inloggad";
 }
@@ -30,9 +32,30 @@ if($num_rows > 0){
 //     case 3:
 //         echo 'Administratör';
 //         break;
-    
+
 //     default:
 //         # code...
 //         break;
 // }
 ?>
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    if(isset($_SESSION["inloggad"]) && $_SESSION["inloggad"]) {
+        echo "Inloggad";
+        echo '<form action="boka.php" method="post"><label>Starttid:</label><input name="starttid" type="datetime-local">
+        <label>Sluttid:</label><input name="sluttid" type="datetime-local"><input value="Skapa" type="submit"></form>';
+    }
+    ?>
+</body>
+
+</html>
