42a4a5f7c2dd3e9d9e5244efb654c4f3e710d729
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-10-12 13:21:36 +0200

    Add files via upload

--- anslut.php
+++ anslut.php
@@ -1,6 +1,6 @@
 <?php
   $conn = new mysqli('localhost', 'root', '', 'TP');
   if($conn->connect_error){
-    die("Dödligt fel: kan inte ansluta till databasen: ". $conn->connect_error);
+    die("Dödligt fel: det går inte ansluta till databasen: ". $conn->connect_error);
   }
 ?>
