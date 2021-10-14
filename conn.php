d590cf0de2e5cfcabad539ab73341e391cbd660c
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-10-14 10:24:58 +0200

    Add files via upload
    objektorienterat -> procedurellt

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "TP";
+$dbname = "tp";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
