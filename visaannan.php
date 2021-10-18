11cc4d1bf2a7bb6ea569dcdef12be1743fee0744
cloudfoxes <90244758+cloudfoxes@users.noreply.github.com>
2021-10-18 13:28:05 +0200

    spion är nu borttagen.
    hejdå spion (:

---
+++ visaannan.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylee.css">
+    <title>Visa när andra är upptagna</title>
+</head>
+<body>
+    <div class="asd">
+
+   <p> Ange ett ID </p>
+</div>
+    </form>
+    <div class="color">
+    <div>
+    <header class="top"></header>
+    </div>
+
+    <br>
+    
+<form action="visarn.php" method="GET">
+    <input type="number" name="VisarID">
+    <input type="submit" value="submit">
+</form>
+</body>
+<script src="theprovider.js"></script>
+</html>
+
