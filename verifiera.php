816f72155e9b8f76caec1c02af652bcfe69d77ba
StefanFolkesson <stefan.folkesson@ntig.se>
2021-10-20 15:06:50 +0200

    Stefan hackar lite

---
+++ verifiera.php
@@ -0,0 +1,17 @@
+funcition verifiera ($key,$user){
+    // ta key och id
+// Kolla så key stämmer med id.
+// om så uppdatera datetime på Användare
+// skicka tillbaka ett true så vi kan fortsätta php filen
+//annrs skicka ett false
+
+    return false;
+}
+
+/********************************************************************** */
+// Så här kommer varje phpfil se ut typ.
+if(verifiera($_GET['key'],$GET_['user'])){
+//gör det ni skall 
+} else {
+//denna anvndare är inte verifierad.
+}
