b297f2ec3c9a0ee6f41ba44b7bd4c529b6c9ecef
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-10-22 09:41:33 +0200

    Skapa lås tabort blogg

---
+++ tabortblogg.php
@@ -0,0 +1,24 @@
+<?php
+    require_once "behorighet.php";
+    require_once "conn.php";
+
+    if(isset($behorighet))
+        if($behorighet == 1) {
+            if(isset($_GET['bloggid'])) {
+                $bloggid = $_GET['bloggid'];
+
+                $sql = "DELETE FROM blogg WHERE BloggID = $bloggid";
+                
+                $blogg = array();
+                if (mysqli_query($conn, $sql)) {
+                    array_push($blogg, array(
+                        "Result"=>true,
+                    ));
+                } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+                echo json_encode($blogg);
+                mysqli_close($conn);
+            }
+        }
+?>
