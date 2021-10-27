5ca79d347e059130d41e80ba8849eff01639085b
cloudfoxes <90244758+cloudfoxes@users.noreply.github.com>
2021-10-27 09:32:25 +0200

    Min uppdatering.
    ja.

--- vissablogg.php
+++ vissablogg.php
@@ -13,6 +13,15 @@ require_once "conn.php";
        $dat = array("Blogg titel"=>$row["Titel"], "Beskrivning"=>$row["Beskrivning"], "skapparen av bloggen"=>$row["AnvandarID"]);
      echo json_encode($dat);
       }
+    }
+
+    $sql= "SELECT InlaggID, Rubrik, Innehall FROM meddelande WHERE BloggID=$bloggid";
+    $result = mysqli_query($conn,$sql);
+    if (mysqli_query($conn, $sql)) {
+        while($row = mysqli_fetch_array($result)) {
+       $dat = array("Inläggets ID"=>$row["InlaggID"], "Rubrik"=>$row["Rubrik"], "Innehåll"=>$row["Innehall"]);
+     echo json_encode($dat);
+      }
     } 
 
 
