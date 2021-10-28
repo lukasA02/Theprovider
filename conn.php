c06e7f656a63ba759bee3ec6aba05db7ddd84bc6
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-10-28 14:14:22 +0200

    Add files via upload

--- conn.php
+++ conn.php
@@ -6,6 +6,8 @@ $dbname = "tp";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+// UTF-8 🥰
+mysqli_set_charset($conn, "utf8mb4");
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
