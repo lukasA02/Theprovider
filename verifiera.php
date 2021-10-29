994794656333d938691f47ce9456b3f21c703be3
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-10-29 09:03:15 +0200

    Add files via upload

--- verifiera.php
+++ verifiera.php
@@ -2,21 +2,26 @@
 require_once "conn.php";
 
 function verifiera($key,$aid) {
-$sql = "SELECT AnvandarID, Behorighet, Anvnamn, Hashkey, Inloggtid FROM anvandare WHERE 
+$sql = "SELECT AnvandarID, Behorighet, Anvnamn, Hashkey, Inloggtid FROM anvandare WHERE
 AnvandarID = $aid && Hashkey = $key && Inloggtid > DATE_SUB(now(), INTERVAL 10 MINUTE)";
 
 // ?????????
 global $conn;
+global $behorighet;
 $result = mysqli_query($conn, $sql);
 
 if(mysqli_num_rows($result) == 1) {
+    while($row = mysqli_fetch_assoc($result))
+        $behorighet = $row['Behorighet'];
+
     $tid = new DateTime('now');
     date_default_timezone_set("Europe/Stockholm");
     $tid = date('Y-m-d H:i:s');
-    
+
+
     $sql = "UPDATE anvandare SET Inloggtid = '$tid' WHERE AnvandarID = $aid";
     $result = mysqli_query($conn, $sql);
-    
+
     return true;
 }
     else {
