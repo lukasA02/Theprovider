6e292ed5080cef03caf79448ed399156d7b3861b
lukasA02 <90241603+lukasA02@users.noreply.github.com>
2021-11-10 09:37:33 +0100

    Add files via upload

---
+++ aterstall.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once '../conn.php';
+
+if(isset($_GET['anv']) && isset($_GET['losen'])) {
+    $username = $_GET['anv'];
+    $password = $_GET['losen'];
+    echo $password."<br>";
+    $password = MD5($password);
+    echo $password;
+}
+
+$sql = "SELECT AnvandarID FROM anvandare WHERE Anvnamn = '$username' && Losen = '$password'";
+$result = mysqli_query($conn, $sql);
+
+echo "ditt id: " . $result -> num_rows;
+$DittId = $result -> num_rows;
+
+?>
+
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylee.css">
+    <title>Ändra ditt lösenord</title>
+</head>
+<body>
+    <div class="asd">
+
+   <p> Ange ditt ID </p>
+</div>
+    </form>
+    <div class="color">
+    <div>
+    <header class="top"></header>
+    </div>
+
+    <br>
+    
+<form action="aterstallt.php" method="GET">
+    <input type="number" name="DittID" value="<?php echo $DittId?>">
+    Ange ditt nya lösenord
+    <input type="text" name="LosenTxt">
+    <input type="text" name="aid" placeholder="AnvändarID">
+    <input type="text" name="hash" placeholder="hash key">
+    <input type="submit" value="submit">
+</form>
+</body>
+
+ 
+
+<script src="theprovider.js"></script>
+</html>
+
---
+++ aterstallt.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once '../conn.php';
+require_once '../verifiera.php';
+
+if(isset($_GET['aid']) && isset($_GET['hash'])){
+
+  if(verifiera($_GET['hash'],$_GET['aid'])){
+
+if(isset($_GET['anv']) && isset($_GET['losen'])) {
+    $username = $_GET['anv'];
+    $password = $_GET['losen'];
+    echo $password."<br>";
+    $password = MD5($password);
+    echo $password;
+}
+
+if(isset($_GET['LosenTxt'])) {
+
+    $NyttLosen = $_GET['LosenTxt'];
+    echo "nytt lösenord: " . $NyttLosen;
+    
+
+} else header('Location: aterstall.php');
+
+$uppercase = preg_match('@[A-Z]@', $NyttLosen);
+$lowercase = preg_match('@[a-z]@', $NyttLosen);
+
+
+$sql = "UPDATE anvandare SET Losen = MD5('$NyttLosen') WHERE  anvandarid = 1";
+
+
+if(!$uppercase || !$lowercase || strlen($NyttLosen) < 25) {
+  echo 'Password should be at least 8 characters in length and should include at least one upper case letter, one number character.';
+}else{
+  echo 'Strong password.';
+  if (mysqli_query($conn, $sql)) {
+  } else {
+    echo "Fel: " . $sql . "<br>" . mysqli_error($conn);
+  }
+}
+
+}else{
+  echo "fuck you 2, skriv in rätt aid/hash";
+}
+}else{
+echo "fuck you, logga in";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylee.css">
+    <title>Ditt lösenord är ändrat</title>
+</head>
+<body>
+
+<script src="theprovider.js"></script>
+</html>
+
