1eafa995e0aef08bc2ad267b758fc410a2806aef
Vaniiis <92022447+Vaniiis@users.noreply.github.com>
2021-11-17 10:01:08 +0100

    Add files via upload

--- redigeraanvandare.php
+++ redigeraanvandare.php
@@ -3,39 +3,69 @@
 require_once "conn.php";
 require_once 'verifiera.php';
 
-echo $_GET['anvandarid'];
+//echo $_GET['anvandarid'];
 if(isset($_GET['aid']) && isset($_GET['hash'])){
 
   if(verifiera($_GET['hash'],$_GET['aid'])){
 
-if(isset($_GET["Anvnamn"]) && isset($_GET["Enamn"]) && isset($_GET["Fnamn"]) && isset($_GET["Epost"]) && isset($_GET["Telefon"])){
-    $anvandarid = $_GET['anvandarid'];
-    $Anvnamn = $_GET['Anvnamn'];
-    $Enamn = $_GET['Enamn'];
-    $Fnamn = $_GET['Fnamn'];
-    $Epost = $_GET['Epost'];
-    $Telefon = $_GET['Telefon'];
+    if (isset ($_GET ["anvandarid"])){
+      $anvandarid = $_GET['anvandarid'];
+      if (isset ($_GET ["Anvnamn"]))
+      $Anvnamn = $_GET['Anvnamn'];
+      if (isset ($_GET ["Enamn"]))
+      $Enamn = $_GET['Enamn'];
+      if (isset ($_GET ["Fnamn"]))
+      $Fnamn = $_GET['Fnamn'];
+      if (isset ($_GET ["Epost"]))
+      $Epost = $_GET['Epost'];
+      if (isset ($_GET ["Telefon"]))
+      $Telefon = $_GET['Telefon'];
+     
+    
+    
+    $sql = "UPDATE anvandare SET ";
+      if(isset ($Anvnamn))
+      $sql = $sql . "Anvnamn = '$Anvnamn',";
+      if(isset ($Enamn))
+      $sql = $sql . "Enamn = '$Enamn',";
+      if(isset ($Fnamn))
+      $sql = $sql . "Fnamn = '$Fnamn',";
+      if(isset ($Epost))
+      $sql = $sql . "Epost = '$Epost',";
+      if(isset ($Telefon))
+      $sql = $sql . "Telefon = '$Telefon',";
+      if(isset ($locked))
+      $sql = $sql . "locked = '$locked',";
+      $len = strlen($sql);
+      $sql[$len-1] = " ";
+      $sql = $sql."WHERE AnvandarID = $anvandarid";
+
+   
+    
+    if (mysqli_query($conn, $sql)) {
+      $output = "användare uppdaterad"; 
+      echo json_encode($output);
+        
+      } else {
+        $output =  " Fel: " . $sql . "<br>" . mysqli_error($conn);
+        echo json_encode($output); 
+      }
+    mysqli_close($conn);
 }
 else{
-    header('Location: index.php');
+   $output = "wrong input, see documentation"; 
+   echo json_encode($output);
 }
 
 
-if(isset($_GET["Anvnamn"]) && isset($_GET["Enamn"]) && isset($_GET["Fnamn"]) && isset($_GET["Epost"]) && isset($_GET["Telefon"])){
-    $sql = "UPDATE anvandare SET Anvnamn = '$Anvnamn', Enamn ='$Enamn', Fnamn='$Fnamn', Epost='$Epost', Telefon='$Telefon' WHERE  anvandarid = $anvandarid";
-}
-
-if (mysqli_query($conn, $sql)) {
-    echo " Anvandare uppdaterad";
-  } else {
-    echo " Fel: " . $sql . "<br>" . mysqli_error($conn);
+  } else{
+    $output = "wrong aid/hash";
+    echo json_encode($output);
+    
   }
-mysqli_close($conn);
-  }else{
-    echo "fuck you 2, skriv in rätt aid/hash";
-  }
-}else{
-  echo "fuck you, logga in";
+} else{
+  $output = "fel"; 
+  echo json_encode($output);
 }
 
 ?>
